d2731871727e92ac660a141da59c5fe2711c44ae
medhatmebed <medhat.mebed@gmail.com>
2024-01-25 16:54:47 +0200

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Medhat Mebed on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
