c7a7511a65e2adc313821f4805893eb61d8f5097
medhatmebed <medhat.mebed@gmail.com>
2024-01-28 19:22:53 +0200

    dynamic regions

--- AirbnbClone/Core/Listings/View/ListingDetailView.swift
+++ AirbnbClone/Core/Listings/View/ListingDetailView.swift
@@ -11,6 +11,13 @@ import MapKit
 struct ListingDetailView: View {
     @Environment(\.dismiss) var dismiss
     let listing: Listing
+    @State private var cameraPosition: MapCameraPosition
+    
+    init(listing: Listing) {
+        self.listing = listing
+        let region = MKCoordinateRegion(center: listing.city == "Los Angeles" ? .losAngeles : .miami, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        self._cameraPosition = State(initialValue: .region(region))
+    }
     
     var body: some View {
         ScrollView {
@@ -158,12 +165,12 @@ struct ListingDetailView: View {
             .padding()
             
             Divider()
-            
+            // MapView
             VStack(alignment: .leading, spacing: 16){
                 Text("Where you'll be")
                     .font(.headline)
                 
-                Map()
+                Map(position: $cameraPosition)
                     .frame(height: 200)
                     .clipShape(RoundedRectangle(cornerRadius: 12))
             }
